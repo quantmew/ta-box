231cc4a663694fdef0ec52006be852e84a99a1ca
Jun Wang <1103870790@qq.com>
2022-04-14 19:25:35 +0800

    update max benchmark examples

--- benchmark/bench_max.py
+++ benchmark/bench_max.py
@@ -16,23 +16,21 @@ from talib import MAX as that_MAX
 def bench_this_max():
     close = np.arange(1000000, 10, -1, dtype=np.float64)
     this_ret = this_MAX(close, timeperiod=500)
-    '''
+
     for i in range(10000, 10500):
         for t in [2, 3, 5, 10, 30, 50]:
             close = np.random.random(i)
             this_ret = this_MAX(close, timeperiod=t)
-    '''
 
 @bench
 def bench_that_max():
     close = np.arange(1000000, 10, -1, dtype=np.float64)
     this_ret = that_MAX(close, timeperiod=500)
-    '''
+
     for i in range(10000, 10500):
         for t in [2, 3, 5, 10, 30, 50]:
             close = np.random.random(i)
             that_ret = that_MAX(close, timeperiod=t)
-    '''
 
 if __name__ == '__main__':
     bench_this_max()
